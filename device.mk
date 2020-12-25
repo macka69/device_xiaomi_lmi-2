284497c9eb6a22be501f8d5b31e3eb71a80dc93f
llololo <llololo@google.com>
2020-12-25 07:47:33 +0000

    lmi: Remove A2DP input module in audio policy configuration
    No Pixel device support A2DP sink functionality.
    
    Bug: 163050296
    Test: A2DP related test.
    Change-Id: I269220cf29451ea79362fc004fa073713f72fdb2

--- device.mk
+++ device.mk
@@ -121,7 +121,6 @@ PRODUCT_COPY_FILES += \
     hardware/qcom-caf/sm8250/audio/configs/common/bluetooth_qti_hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_qti_hearing_aid_audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
