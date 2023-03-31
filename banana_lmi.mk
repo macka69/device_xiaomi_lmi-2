516a79dd24d5737ac3eeb102a261d2005d1bb34d
John Macka <macka2004@gmail.com>
2023-03-31 21:28:39 +1000

    lmi: update blobs to V14.0.1.0.SJKMIXM

--- banana_lmi.mk
+++ banana_lmi.mk
@@ -32,4 +32,4 @@ BANANA_MAINTAINER := Macka
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Redmi/lmi/lmi:12/RKQ1.211001.001/V13.0.3.0.SJKMIXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/lmi/lmi:12/SKQ1.211006.001/V14.0.1.0.SJKMIXM:user/release-keys
