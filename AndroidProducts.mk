0cb625ca9c73965eab9a2ec6e6a7e865458785aa
John Macka <macka2004@gmail.com>
2023-06-02 14:34:33 +1000

    lmi: bring-up arrowos 13.1

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The xtendedOS Project
+# Copyright (C) 2021 The arrowOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_lmi.mk
+    $(LOCAL_DIR)/arrow_lmi.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_lmi-eng \
-    xtended_lmi-user \
-    xtended_lmi-userdebug
+    arrow_lmi-eng \
+    arrow_lmi-user \
+    arrow_lmi-userdebug
