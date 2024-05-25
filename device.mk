5788929e1fe5d4c28250fdd92f43799caf7f57b4
someone5678 <someone5678@users.noreply.github.com>
2024-05-25 15:47:19 +0900

    lmi: Signing build with custom key
    Change-Id: Idd44da5142faf0fcdc414136298ee2a2c5ad3888

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Signing build
+TARGET_BUILD_FULLY_SIGN := true
+
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
