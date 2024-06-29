51014463a1c1f524d4d70de1b73d89206038231e
Jis G Jacob <studiokeys@blissroms.org>
2024-06-29 13:19:53 -0400

    lmi: Build `android.hardware.audio.sounddose-vendor-impl.so`
    E audiohalservice: Failed to dlopen android.hardware.audio.sounddose-vendor-impl.so: dlopen failed: library "android.hardware.audio.sounddose-vendor-impl.so" not found
    
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- device.mk
+++ device.mk
@@ -94,6 +94,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl \
+    android.hardware.audio.sounddose-vendor-impl \
     audio.bluetooth.default \
     audio.primary.kona \
     audio.r_submix.default \
