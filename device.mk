4340e894ae70baa28810412b2c8c4459d964f12f
Sugakesshaa <sugakesshaa@pixelos.net>
2025-02-05 19:12:48 +0800

    lmi: Import Dolby C2 Media service
    from OnePlus 11 CPH2447_15.0.0.401(EX01)
    
    Change-Id: Icdfc5b11c7635d0707c2a39f54e0c8d149c771bc

--- device.mk
+++ device.mk
@@ -288,6 +288,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
+    media_codecs_c2_dolby_audio.xml \
     media_codecs_performance_c2.xml \
     video_system_specs.json
 
