Kyuofox-Studio/android_device_xiaomi_lmi

lmi: remove OMX
 * OMX HW video codecs are no longer used after enabling Codec2
 * OMX HW audio codecs won't work without QCOM A/V enhancements, which
   we don't have on Lineage.

Change-Id: I65852151c8fb6387add5fc4402ba3f13c97b5f64

device.mk
@@ -226,12 +226,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
-    media_codecs_kona.xml \
     media_codecs_performance_c2.xml \
-    media_codecs_performance_kona.xml \
     media_profiles_kona.xml \
-    media_profiles_V1_0.xml \
-    system_properties.xml
+    media_profiles_V1_0.xml
 
 # Native Public Libraries
 PRODUCT_PACKAGES += \
@@ -260,22 +257,13 @@ PRODUCT_PACKAGES += \
     libnfc-nxp.conf
 
 # OMX
+TARGET_SUPPORTS_OMX_SERVICE := false
+
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
     libcodec2_hidl@1.0.vendor \
     libcodec2_vndk.vendor \
-    libmm-omxcore \
-    libOmxAacEnc \
-    libOmxAmrEnc \
-    libOmxCore \
-    libOmxEvrcEnc \
-    libOmxG711Enc \
-    libOmxQcelp13Enc \
-    libOmxVdec \
-    libOmxVenc \
-    libstagefrighthw \
-    libstagefright_omx.vendor \
-    libstagefright_softomx_plugin.vendor
+    libmm-omxcore
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage

macka69/device_xiaomi_lmi-2

device.mk
@@ -288,9 +288,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
-    media_codecs_performance_c2.xml \
-    media_codecs_kona.xml \
-    media_codecs_performance_kona.xml
+    media_codecs_performance_c2.xml
 
 # NFC
 PRODUCT_PACKAGES += \
@@ -324,22 +322,8 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/xiaomi
 
 # OMX
-PRODUCT_PACKAGES += \
-    libc2dcolorconvert \
-    libcodec2_hidl@1.0.vendor \
-    libcodec2_vndk.vendor \
-    libmm-omxcore \
-    libOmxAacEnc \
-    libOmxAmrEnc \
-    libOmxCore \
-    libOmxEvrcEnc \
-    libOmxG711Enc \
-    libOmxQcelp13Enc \
-    libOmxVdec \
-    libOmxVenc \
-    libstagefrighthw \
-    libstagefright_omx.vendor \
-    libstagefright_softomx_plugin.vendor
+TARGET_SUPPORTS_OMX_SERVICE := false
+
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage