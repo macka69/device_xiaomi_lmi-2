Kyuofox-Studio/android_device_xiaomi_lmi

lmi: video: enbale adaptive b-frame capability
'enc_default_bframe_count' set to 1.
'enc_max_mbs_per_sec' set to max-hw-load.

Bug: 156724722
Test: run ECO experiments
Change-Id: I884be887e9ae71bb9f8778f8bd0f5dd728d1ba7e

device.mk
@@ -228,7 +228,8 @@ PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
     media_codecs_performance_c2.xml \
     media_profiles_kona.xml \
-    media_profiles_V1_0.xml
+    media_profiles_V1_0.xml \
+    video_system_specs.json
 
 # Native Public Libraries
 PRODUCT_PACKAGES += \

macka69/device_xiaomi_lmi-2

device.mk
@@ -289,7 +289,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
-    media_codecs_performance_c2.xml
+    media_codecs_performance_c2.xml \
+    video_system_specs.json
 
 # NFC
 PRODUCT_PACKAGES += \